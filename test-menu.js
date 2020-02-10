2c918bdf8243b3a6066eaf493ef8d2d5b9d94b3d
askja7 <staneva.lora@gmail.com>
2020-02-10 20:53:12 +0100

    adding-price-calculation

--- test-menu.js
+++ test-menu.js
@@ -28,6 +28,21 @@ function showSingleDish(dish) {
   copy.querySelector("h3.dish-name").textContent = dish.name;
     copy.querySelector(".description span").textContent = dish.shortdescription;
     copy.querySelector(".price span").textContent = dish.price;
+    //calculating price
+    if (dish.discount) { //on sale
+    copy.querySelector(".price-discount span").textContent = dish.price;
+    const newPrice = Math.round(dish.price - dish.price * dish.discount / 100);
+
+    copy.querySelector(".price-full span").textContent = newPrice;
+    //calculate new price
+    //49-49*10/100
+    //dish.price-dish.price*price.discount/100
+  } else { // not on discount
+    copy.querySelector(".price-discount").remove()
+    copy.querySelector(".price-full span").textContent = dish.price
+  }
+
+
     //copy.querySelector(".region span").textContent = dish.region;
 
     //importing images from database
